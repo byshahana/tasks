b851e66f46b110f91647eafc8e96fb808a6775aa
byshahana <byshahana.in@gmail.com>
2026-06-02 12:27:53 +0530

    Add files via upload

---
+++ contact_book.py
@@ -0,0 +1,3 @@
+contacts={"shahana:"9845938202,"adila:"9089765443}
+get_name=(contacts.get(name))   
+update_name=(contact.update({}))      
---
+++ dict,str_tasks.py
@@ -0,0 +1,11 @@
+sentences="apple mango grape mango apple"
+words=sentences.split()
+count={}
+for word in words:
+    if word in count:
+        count[word]+=1
+    else:
+        count[word]=1
+print(count)
+
+
